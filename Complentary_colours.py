4651eb2864409ae1f9ad53e8166f87d7fad366ee
trupti <tnkolte@gmail.com>
2020-04-09 11:28:33 +0100

    RGB complementary colour codes

---
+++ Complentary_colours.py
@@ -0,0 +1,10 @@
+red = int(input("Red value: "))
+green = int(input("Green value: "))
+blue = int(input("Blue value: "))
+
+compred = 225 - red
+compgreen = 225 - green
+compblue = 225 - blue
+
+print("colour: ",red,green, blue)
+print("complementary colour: ", compred, compgreen,compblue)
