15b9be16ef185fa2a03c9ae12ef76708539f057c
trupti <tnkolte@gmail.com>
2020-04-13 13:28:48 +0100

    swaps the value of 2 variables

---
+++ number_swap.py
@@ -0,0 +1,7 @@
+number1 = int(input("Enter a number between 1-10: "))
+number2 = int(input("Enter another number between 1-10: "))
+number1 = number1 + number2
+number2 = number1 - number2
+number1 = number1 - number2
+print("The first number is: ",number1)
+print("The second number is: ",number2)
