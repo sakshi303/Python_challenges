04b5982b80df222fa331b72a545738c264583c5e
trupti <tnkolte@gmail.com>
2020-04-14 10:57:35 +0100

    happy new year!

--- happy_new_year.py
+++ happy_new_year.py
@@ -1,14 +1,8 @@
-
 import time
-
 message = '] HAPPY NEW YEAR ['
-
-
 for counter in range(5,0,-1):
     print(counter)
     time.sleep(1)
-
-
 
 print("==================")
 print("]                [")
